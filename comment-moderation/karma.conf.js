d289182667b68ce4fee6c014f65bfd2f67b643bf
Maxim Hüsler <maxim.huesler@2sic.com>
2022-03-18 15:25:39 +0100

    naming

--- comment-moderation/karma.conf.js
+++ comment-moderation/karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/comment-management'),
+      dir: require('path').join(__dirname, './coverage/comment-moderation'),
       subdir: '.',
       reporters: [
         { type: 'html' },
--- comment-moderation/src/app/app.component.spec.ts
+++ comment-moderation/src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'comment-management'`, () => {
+  it(`should have as title 'comment-moderation'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('comment-management');
+    expect(app.title).toEqual('comment-moderation');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('comment-management app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('comment-moderation app is running!');
   });
 });
