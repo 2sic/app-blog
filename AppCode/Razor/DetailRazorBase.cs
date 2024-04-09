1735854c158d977b51bda65f6ff93566191cd243
Dominik Graf <dominik.graf@2sic.com>
2024-04-09 11:25:02 +0200

    new tag > blogTag

--- AppCode/Razor/DetailRazorBase.cs
+++ AppCode/Razor/DetailRazorBase.cs
@@ -5,68 +5,68 @@ using System;
 
 namespace AppCode.Razor
 {
-  // /// <summary>
-  // /// Base Class for Razor Views which have a typed App but don't use the Model or use the typed MyModel.
-  // /// </summary>
-  // public abstract class DetailRazorBase : AppRazor<object>
-  // {
-  //   public IHtmlTag PostMicroPreview(ITypedItem post, string context) {
-  //   if (post == null) return null;
-  //   var links = GetCode("./helpers/Links.cs");
-  //   var title = App.Resources.String(context == "previous" ? "PreviousPost" : "NextPost");
+  /// <summary>
+  /// Base Class for Razor Views which have a typed App but don't use the Model or use the typed MyModel.
+  /// </summary>
+  public abstract class DetailRazorBase : AppRazor<object>
+  {
+    public IHtmlTag PostMicroPreview(ITypedItem post, string context) {
+    if (post == null) return null;
+    var links = GetCode("./helpers/Links.cs");
+    var title = App.Resources.String(context == "previous" ? "PreviousPost" : "NextPost");
 
-  //   var imgUrl = post.Url("Image");
-  //   return Tag.Div().Class(context).Wrap(
-  //     Tag.A().Href(links.LinkToDetailsPage(post)).Wrap(
-  //       (Text.Has(imgUrl) 
-  //         ? post.Picture("Image", settings: "NextPost", imgAltFallback: post.String("Title"), imgClass: "rounded-circle d-none d-lg-block")
-  //         : null),
-  //       Tag.Span(
-  //         Tag.Strong(title) + " " + post.String("Title")
-  //       ).Class("app-blog-previouslink-title")
-  //     )
-  //   );
-  // }
+    var imgUrl = post.Url("Image");
+    return Tag.Div().Class(context).Wrap(
+      Tag.A().Href(links.LinkToDetailsPage(post)).Wrap(
+        (Text.Has(imgUrl) 
+          ? post.Picture("Image", settings: "NextPost", imgAltFallback: post.String("Title"), imgClass: "rounded-circle d-none d-lg-block")
+          : null),
+        Tag.Span(
+          Tag.Strong(title) + " " + post.String("Title")
+        ).Class("app-blog-previouslink-title")
+      )
+    );
+  }
 
-  // public IHtmlTag BackToListButton() {
-  //   return Tag.Div().Class("backlink").Wrap(
-  //     Tag.A(App.Resources.String("BackToHome"))
-  //       .Class("btn btn-outline-primary")
-  //       .Href(Link.To())
-  //   );
-  // }
+  public IHtmlTag BackToListButton() {
+    return Tag.Div().Class("backlink").Wrap(
+      Tag.A(App.Resources.String("BackToHome"))
+        .Class("btn btn-outline-primary")
+        .Href(Link.To())
+    );
+  }
 
-  // public void AddMetaTags(ITypedItem post) {
+  public void AddMetaTags(ITypedItem post) {
     
-  //   var metaImageUrl = Text.Has(post.Url("Image"))
-  //       ? Link.Image(post.Url("Image"), settings: "Blog", type: "full")
-  //       : "";
+    var metaImageUrl = Text.Has(post.Url("Image"))
+        ? Link.Image(post.Url("Image"), settings: "Blog", type: "full")
+        : "";
 
-  //   var teaser = post.String("Teaser", scrubHtml: true);
-  //   var sharingDescription = Text.First(post.String("SharingDescription"), teaser);
+    var teaser = post.String("Teaser", scrubHtml: true);
+    var sharingDescription = Text.First(post.String("SharingDescription"), teaser);
 
-  //   // Try to replace the term "PostTitle" in the page title with the post title, otherwise prefix the existing title
-  //   Kit.Page.SetTitle(Text.First(post.String("MetaTitle"), post.String("Title")) + " ", "PostTitle");
+    // Try to replace the term "PostTitle" in the page title with the post title, otherwise prefix the existing title
+    Kit.Page.SetTitle(Text.First(post.String("MetaTitle"), post.String("Title")) + " ", "PostTitle");
 
 
-  //   Kit.Page.SetDescription(Text.First(post.String("MetaDescription"), teaser));
+    Kit.Page.SetDescription(Text.First(post.String("MetaDescription"), teaser));
 
-  //   // Add open graph meta information
-  //   var title = post.String("Title");
-  //   Kit.Page.AddOpenGraph("og:type", "article");
-  //   Kit.Page.AddOpenGraph("og:title", title);
-  //   Kit.Page.AddOpenGraph("og:site_name", App.Resources.String("BlogTitle"));
-  //   Kit.Page.AddOpenGraph("og:url", Link.To(parameters: "details=" + post.String("UrlKey")));
-  //   Kit.Page.AddOpenGraph("og:description", sharingDescription);
-  //   Kit.Page.AddOpenGraph("og:image", metaImageUrl);
-  //   Kit.Page.AddOpenGraph("og:image:height", "1200");
-  //   Kit.Page.AddOpenGraph("og:image:width", "630");
+    // Add open graph meta information
+    var title = post.String("Title");
+    Kit.Page.AddOpenGraph("og:type", "article");
+    Kit.Page.AddOpenGraph("og:title", title);
+    Kit.Page.AddOpenGraph("og:site_name", App.Resources.String("BlogTitle"));
+    Kit.Page.AddOpenGraph("og:url", Link.To(parameters: "details=" + post.String("UrlKey")));
+    Kit.Page.AddOpenGraph("og:description", sharingDescription);
+    Kit.Page.AddOpenGraph("og:image", metaImageUrl);
+    Kit.Page.AddOpenGraph("og:image:height", "1200");
+    Kit.Page.AddOpenGraph("og:image:width", "630");
 
-  //   // Add twitter meta information
-  //   Kit.Page.AddMeta("twitter:card", "summary_large_image");
-  //   Kit.Page.AddMeta("twitter:title", title);
-  //   Kit.Page.AddMeta("twitter:description", sharingDescription);
-  //   Kit.Page.AddMeta("twitter:image", metaImageUrl);
-  // }
-  // }
+    // Add twitter meta information
+    Kit.Page.AddMeta("twitter:card", "summary_large_image");
+    Kit.Page.AddMeta("twitter:title", title);
+    Kit.Page.AddMeta("twitter:description", sharingDescription);
+    Kit.Page.AddMeta("twitter:image", metaImageUrl);
+  }
+  }
 }
