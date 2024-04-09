1a44568a2eb9cefeab06f689d939871ebdcf52a5
Dominik Graf <dominik.graf@2sic.com>
2024-04-09 10:04:17 +0200

    details type wip

---
+++ AppCode/Razor/AppRazor.cs
@@ -0,0 +1,12 @@
+using AppCode.Data;
+using ToSic.Razor.Blade;
+using ToSic.Sxc.Data;
+using System;
+
+namespace AppCode.Razor
+{
+  public abstract partial class AppRazor<TModel> : Custom.Hybrid.RazorTyped<TModel>
+  {
+  
+  }
+}
