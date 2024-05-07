b51bfa5c491f3010ae5135bf0de3f4cb45080613
Dominik Graf <dominik.graf@2sic.com>
2024-05-07 07:54:23 +0200

    todos fix

--- AppCode/Razor/AppRazor.cs
+++ AppCode/Razor/AppRazor.cs
@@ -6,17 +6,6 @@ namespace AppCode.Razor
   public abstract partial class AppRazor<TModel> : Custom.Hybrid.RazorTyped<TModel>
   {
     /// <summary>
-    /// Returns a teaser for a blog post
-    /// </summary>
-    // TODO: @2dg - move this to the BlogPost class as an additional method, since that's where it belongs
-    public string BuildTeaser(BlogPost post)
-    {
-      // TODO: @2dg - use post.String("Content", scrub: true)
-      // return Text.First(post.Teaser, Text.Ellipsis(post.String("Content", scrub: true), 100));
-         return Text.First(post.Teaser, Text.Ellipsis(Kit.Scrub.All(post.Content), 100));
-    }
-
-    /// <summary>
     /// Returns a safe url to a post details page
     /// </summary>
     public string LinkToDetailsPage(BlogPost post)
